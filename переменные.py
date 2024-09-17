de55a43264c66aab04596ec9068006ccfbfec23d
yanmazur <zacheper102@gmail.com>
2024-09-17 11:59:49 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,4 @@
+Home_work = 12
+Hours_spent = 1.5
+Well = 'Python'
+print("Курс:",  Well, "Всего задач:", Home_work, "Затрачено часов:", Hours_spent, "Среднее время выполнения:", Hours_spent/Home_work)
